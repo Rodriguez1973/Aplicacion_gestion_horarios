2718c39b3778fbe8d91a30dd90b13bf2a3026a41
Rodriguez <j062667@gmail.com>
2022-11-11 22:50:23 +0100

    Se codifica grabar datos.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ let horarioMap = new Map() //Map con los datos del horario.
 const profesores = ['Marian', 'Alvaro', 'Fernando', 'David'] //Declaración de los profesores.
 const asignaturas = ['DWES', 'DWEC', 'DI', 'DAW'] //Declaración de las asignaturas.
 
-window.onload=()=>{
+window.onload = () => {
   dibujarTabla();
 }
 
@@ -22,7 +22,7 @@ const iHora = document.getElementById('iHora')
 
 //--------------------------------------------------------------------------------------------------
 //Escuchadores de los eventos.
-bReiniciar.addEventListener('click', reiniciarDatos, false) //Se produce al hacer click sobre el botón bReiniciar.
+bReiniciar.addEventListener('click', reiniciarHorario, false) //Se produce al hacer click sobre el botón bReiniciar.
 bGrabar.addEventListener('click', grabarDatos, false) //Se produce al hacer click sobre el botón bReiniciar.
 iProfesor.addEventListener('blur', validaProfesor, false) //Se produce cuando iProfesor pierde el foco.
 iAsignatura.addEventListener('blur', validaAsignatura, false) //Se produce cuando iAsignatura pierde el foco.
@@ -104,17 +104,24 @@ function borrarAsignatura() {
 //--------------------------------------------------------------------------------------------------
 //Clase HorarioAsignatura.
 class HorarioAsignatura {
-    constructor(profesor, asignatura, diaSemana, hora) {
-        this.profesor = profesor
-        this.asignatura = asignatura
-        this.diaSemana = diaSemana
-        this.hora = hora
-    }
+  constructor(profesor, asignatura, diaSemana, hora) {
+    this.profesor = profesor
+    this.asignatura = asignatura
+    this.diaSemana = diaSemana
+    this.hora = hora
+  }
 }
 
 //--------------------------------------------------------------------------------------------------
 //Función que reinicia los datos de la tabla.
-function reiniciarDatos() {}
+function reiniciarHorario() {
+  horarioMap.clear;
+  iProfesor.value=""
+  iAsignatura.value=""
+  iDiaSemana.value=1
+  iHora.value=1
+  dibujarTabla()
+ }
 
 //--------------------------------------------------------------------------------------------------
 //Función que graba los datos.
@@ -128,37 +135,65 @@ function grabarDatos(evt) {
       iDiaSemana.value,
       iHora.value,
     )
+
+    //Crear key.
+    let key='c'+iHora.value+iDiaSemana.value
+    console.log(key)
+    horarioMap.set (key, hProfAsig);
+    celda=document.getElementById(key)
+    celda.innerText=hProfAsig.asignatura;
+    crearOyenteClick(key)
   }
 }
 
 //--------------------------------------------------------------------------------------------------
 //Función que dibuja la tabla.
-function dibujarTabla(){
-  let cuerpoTabla=document.getElementById("tabla")
-  for(let f=0; f<=iHora.length; f++){
+function dibujarTabla() {
+  let cuerpoTabla = document.getElementById("tabla")
+  tabla.innerText=""
+  for (let f = 0; f <= iHora.length; f++) {
     //Crea una fila.
-    let fila=document.createElement("tr")
+    let fila = document.createElement("tr")
     //La añade a la tabla.  
     cuerpoTabla.appendChild(fila)
-    for(let c=0;c<=iDiaSemana.length; c++){
+    for (let c = 0; c <= iDiaSemana.length; c++) {
       //Primera fila. Elementos <th>
-      if(f===0){
-        let celda=document.createElement("th")
+      if (f === 0) {
+        let celda = document.createElement("th")
         //Añade el contenido.
-        if(c===0){
-          celda.innerText='Horario'
-        }else{
-          celda.innerText=iDiaSemana[c-1].innerText
+        if (c === 0) {
+          celda.innerText = 'Horario'
+        } else {
+          celda.innerText = iDiaSemana[c - 1].innerText
         }
         fila.appendChild(celda)
-      }else{  //No es la primera fila.
-        let celda=document.createElement("td")
+      } else {  //No es la primera fila.
+        let celda = document.createElement("td")
         //Primera columna añade las horas.
-        if(c===0){
-          celda.innerText=iHora[f-1].innerText
+        if (c === 0) {
+          celda.innerText = iHora[f - 1].innerText
+        } else {
+          celda.id = 'c' + f + c
         }
         fila.appendChild(celda)
       }
     }
   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//Función que crea escuchador del evento click para cada una de las celdas.
+function crearOyenteClick(id) {
+  let celda = document.getElementById(id);
+  celda.addEventListener('click', mostrarDatos, false);
+}
+
+//--------------------------------------------------------------------------------------------------
+function mostrarDatos(evt){
+  console.log(evt.target.id)
+  let horario=horarioMap.get(evt.target.id)
+  iProfesor.value=horario.profesor;
+  iAsignatura.value=horario.asignatura;
+  iDiaSemana.value=horario.diaSemana
+  iHora.value=horario.hora;
 }
