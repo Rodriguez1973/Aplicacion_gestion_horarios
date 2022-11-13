169990eb8d2855b906b517b162e5d6b6294fc74b
Rodriguez1973 <j062667@gmail.com>
2022-11-13 01:13:15 +0100

    Fin de la depuración.

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ Fecha: 06/11/2022
 let horarioMap = new Map() //Map con los datos del horario.
 const profesores = ['Marian', 'Fernando', 'David', 'Alvaro'] //Declaración de los profesores.
 const asignaturas = ['DWES', 'DWEC', 'DI', 'DAW'] //Declaración de las asignaturas.
-let profesorValidado=false //Flag que controla que el profesor está validado.
-let asignaturaValidada=false //Flag que controla que la asignatura está validada.
 
 window.onload = () => {
   dibujarTabla()
@@ -23,7 +21,7 @@ const iDiaSemana = document.getElementById('iDiaSemana')
 const iHora = document.getElementById('iHora')
 
 //--------------------------------------------------------------------------------------------------
-//Escuchadores de los eventos.
+//Oyentes de los eventos.
 bReiniciar.addEventListener('click', reiniciarHorario, false) //Se produce al hacer click sobre el botón bReiniciar.
 bGrabar.addEventListener('click', grabarDatos, false) //Se produce al hacer click sobre el botón bReiniciar.
 iProfesor.addEventListener('blur', validaProfesor, false) //Se produce cuando iProfesor pierde el foco.
@@ -35,21 +33,20 @@ iAsignatura.addEventListener('focus', seleccionarAsignatura, false) //Se produce
 //Función que valida un profesor.
 function validaProfesor() {
   let profesor = iProfesor.value.trim()
-  //Si se ha producido la perdida del foco y la cadena está vacía no la debe validar.
-  if (profesor === '') {
-    profesorValidado=false;
+  //Si se ha producido la perdida del foco y la cadena está vacía, no la debe validar.
+  if (profesor === '' || profesor==="El campo no puede estar vacío.") {
+    return false
     //En el resto de casos valida la cadena.
   } else {
     profesor = profesor.toLowerCase().capitalize()
     //Comprueba si está incluido en el array de profesores.
     if (profesores.includes(profesor)) {
-      profesorValidado=true;
-      validaAsignatura()
+      iProfesor.value = profesor
+      return true
     } else {
       //No se encuentra incluido
       iProfesor.value = 'Profesor no válido.'
       iProfesor.style.color = 'red'
-      profesorValidado=false
     }
   }
 }
@@ -58,32 +55,19 @@ function validaProfesor() {
 //Función que valida una asignatura.
 function validaAsignatura() {
   let asignatura = iAsignatura.value.trim()
-  //Si se ha producido la perdida del foco y la cadena está vacía no la debe validar.
-  if (asignatura === '') {
-    asignaturaValidada=false;
+  //Si se ha producido la perdida del foco y la cadena está vacía, no la debe validar.
+  if (asignatura === '' || asignatura==="El campo no puede estar vacío") {
+    return false
     //En el resto de casos valida la cadena.
   } else {
     asignatura = asignatura.toUpperCase()
     //Comprueba si está incluida en el array de asignaturas.
     if (asignaturas.includes(asignatura)) {
-      let profesor = iProfesor.value.trim().toLowerCase().capitalize()
-      if (profesores.includes(profesor)) {
-        //Si los indices coinciden la asignatura es de ese profesor. Valida la asignatura adjunta al profesor.
-        if (asignaturas.indexOf(asignatura) === profesores.indexOf(profesor)) {
-          asignaturaValidada=true
-        }else{
-          iAsignatura.value = 'Asignatura no válida.'
-          iAsignatura.style.color = 'red'
-          asignaturaValidada=false
-        }
-      }else{
-      //No valida la asignatura porque no es adjunta al profesor.
-        asignaturaValidada=false
-      }
+      iAsignatura.value = asignatura
+      return true
     } else {
       iAsignatura.value = 'Asignatura no válida.'
       iAsignatura.style.color = 'red'
-      asignaturaValidada=false
     }
   }
 }
@@ -92,22 +76,22 @@ function validaAsignatura() {
 //Función que define el nuevo método capitalize para la clase String. Este método devuelve una cadena con la primera letra en mayúsculas y el resto en minúsculas.
 String.prototype.capitalize = function () {
   let cadena = this.toString()
-  try {
-    if (cadena.length >= 2) {
-      return (
-        cadena.substring(0, 1).toUpperCase() + cadena.substring(1).toLowerCase()
-      )
-    } else if (cadena.length === 1) {
-      return cadena.toUpperCase()
-    }
-  } catch (Exception) {
-    return null
+
+  if (cadena.length >= 2) {
+    return (
+      cadena.substring(0, 1).toUpperCase() + cadena.substring(1).toLowerCase()
+    )
+  } else if (cadena.length === 1) {
+    return cadena.toUpperCase()
+  } else {
+    return ''
   }
 }
 
 //--------------------------------------------------------------------------------------------------
 //Función que selecciona todo el contenido de iProfesor y cambia a negro la fuente.
 function seleccionarProfesor() {
+  iProfesor.value = ''
   iProfesor.select()
   iProfesor.style.color = 'black'
 }
@@ -115,6 +99,7 @@ function seleccionarProfesor() {
 //--------------------------------------------------------------------------------------------------
 //Función que selecciona todo el contenido de iAsignatura y cambia a negro la fuente.
 function seleccionarAsignatura() {
+  iAsignatura.value = ''
   iAsignatura.select()
   iAsignatura.style.color = 'black'
 }
@@ -141,26 +126,44 @@ function reiniciarHorario() {
 //--------------------------------------------------------------------------------------------------
 //Función que graba los datos.
 function grabarDatos(evt) {
-  asignaturaValidada=false
-  validaAsignatura()
-  //Si la asignatura y el profesor son válidos, asignatura estará validada
-  if (asignaturaValidada) {
-    //Crea un nuevo horario de una asignatura
-    let hProfAsig = new HorarioAsignatura(
-      iProfesor.value.trim().toLowerCase().capitalize(),
-      iAsignatura.value.trim().toUpperCase(),
-      iDiaSemana.value,
-      iHora.value,
-    )
+  let profesor = iProfesor.value.trim()
+  let asignatura = iAsignatura.value.trim()
+  //Si la asignatura y el profesor son válidos.
+  if (validaProfesor() && validaAsignatura()) {
+    profesor=profesor.toLowerCase().capitalize()
+    asignatura=asignatura.toUpperCase()
+    //El profesor tiene asignada esa asignatura.
+    if (profesores.indexOf(profesor) == asignaturas.indexOf(asignatura)) {
+      //Crea un nuevo horario de una asignatura
+      let hProfAsig = new HorarioAsignatura(
+        profesor,
+        asignatura,
+        iDiaSemana.value,
+        iHora.value,
+      )
 
-    //Crear key.
-    let key = 'c' + iHora.value + iDiaSemana.value
-    //console.log(key) //Clave de la celda y el map. Utilizado en depuración.
-    horarioMap.set(key, hProfAsig) //Añade al map.
-    celda = document.getElementById(key) //Obtenemos la celda por el id
-    celda.innerText = hProfAsig.asignatura //Introduce la asignatura.
-    crearOyenteClick(key) //Crea el oyente del click para esa celda.
-    reiniciarCampos()
+      //Crear key.
+      let key = 'c' + iHora.value + iDiaSemana.value
+      //console.log(key) //Clave de la celda y el map. Utilizado en depuración.
+      horarioMap.set(key, hProfAsig) //Añade al map.
+      celda = document.getElementById(key) //Obtenemos la celda por el id
+      celda.innerText = hProfAsig.asignatura //Introduce la asignatura.
+      crearOyenteClick(key) //Crea el oyente del click para esa celda.
+      reiniciarCampos()
+    } else {
+      iAsignatura.value =
+        iAsignatura.value.toUpperCase() + '. No está asignada al profesor.'
+      iAsignatura.style.color = 'red'
+    }
+  } else {
+    if (profesor === '') {
+      iProfesor.value = 'El campo no puede estar vacío.'
+      iProfesor.style.color = 'red'
+    }
+    if (asignatura === '') {
+      iAsignatura.value = 'El campo no puede estar vacío.'
+      iAsignatura.style.color = 'red'
+    }
   }
 }
 
@@ -186,17 +189,17 @@ function dibujarTabla() {
     }
   }
 
-  //Creamos el tbody y lo añade al cuerpo de la tabla.
+  //Crea el tbody y lo añade al cuerpo de la tabla.
   let tbody = document.createElement('tbody')
   cuerpoTabla.appendChild(tbody)
   //Bucle que genera las filas del cuerpo de la tabla.
   for (let f = 1; f <= iHora.length; f++) {
     let tr = document.createElement('tr')
     tbody.appendChild(tr)
-    //Bucle que genera las celdas.
+    //Bucle que genera las celdas de una fila.
     for (let c = 0; c <= iDiaSemana.length; c++) {
       let celda = document.createElement('td') //Crea la celda
-      tr.appendChild(celda) //Añade la celda a la fila
+      tr.appendChild(celda) //Añade la celda a la fila.
       //Primera columna añade las horas.
       if (c === 0) {
         celda.innerText = iHora[f - 1].innerText
@@ -211,15 +214,16 @@ function dibujarTabla() {
 }
 
 //--------------------------------------------------------------------------------------------------
-//Función que crea escuchador del evento click para cada una de las celdas.
+//Función que crea oyente del evento click para cada una de las celdas.
 function crearOyenteClick(id) {
   let celda = document.getElementById(id)
   celda.addEventListener('click', mostrarDatos, false)
 }
 
 //--------------------------------------------------------------------------------------------------
+//Función que muestra los datos de una celda si se hace click en ella.
 function mostrarDatos(evt) {
-  console.log(evt.target.id)
+  //console.log(evt.target.id) //Depuración.
   let horario = horarioMap.get(evt.target.id)
   iProfesor.value = horario.profesor
   iAsignatura.value = horario.asignatura
@@ -228,7 +232,8 @@ function mostrarDatos(evt) {
 }
 
 //--------------------------------------------------------------------------------------------------
-function reiniciarCampos(){
+//Función que inicializa los campos.
+function reiniciarCampos() {
   iProfesor.value = ''
   iAsignatura.value = ''
   iDiaSemana.value = 1
